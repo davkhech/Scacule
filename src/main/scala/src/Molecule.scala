0163709c5c83bb8974f91581e626d027c3448616
Davit Khechoyan <davkhech@gmail.com>
2017-05-28 10:54:34 +0400

    FIX: molecule getEnergy

--- src/main/scala/src/Molecule.scala
+++ src/main/scala/src/Molecule.scala
@@ -2,17 +2,17 @@ package src
 
 class Molecule(atoms: List[Atom]){
   def getEnergy(eps0: Double, eps1: Double) = {
-    def iter(atoms: List[(Atom, Atom)], d: Set[Atom], s: Set[Atom]): (Set[Atom], Set[Atom]) = atoms match {
-      case Nil ⇒ (d, s)
+    def iter(atoms: List[(Atom, Atom)], dq: Int, d: Set[Atom], s: Set[Atom]): (Int, Set[Atom], Set[Atom]) = atoms match {
+      case Nil ⇒ (dq, d, s)
       case h::t ⇒
-        if (h._1.y == 0 && h._2.y == 0) iter(t, d + h._1 + h._2, s + h._1 + h._2)
-        else if (h._1.y != 0 && h._2.y == 0) iter(t, d, s + h._2)
-        else if (h._1.y == 0 && h._2.y != 0) iter(t, d, s + h._1)
-        else iter(t, d, s)
+        if (h._1.y == 0 && h._2.y == 0) iter(t, dq + 1, d + h._1 + h._2, s + h._1 + h._2)
+        else if (h._1.y != 0 && h._2.y == 0) iter(t, dq, d, s + h._2)
+        else if (h._1.y == 0 && h._2.y != 0) iter(t, dq, d, s + h._1)
+        else iter(t, dq, d, s)
     }
 
-    val (d, s) = iter(atoms zip atoms.tail, Set(), Set())
-    eps0 * d.size / 2 + eps1 * (s.size - d.size)
+    val (dq, d, s) = iter(atoms zip atoms.tail, 0, Set(), Set())
+    eps0 * dq + eps1 * (s.size - d.size)
   }
 
   def getRadialDistance = Math.sqrt(Math.pow(atoms.head.x, 2) + Math.pow(atoms.head.y, 2))
